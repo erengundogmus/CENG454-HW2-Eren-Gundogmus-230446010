0873e247fa42502bdcd400bb133fef22d47dc77d
Eren Gündoğmuş <erengundogmus5@gmail.com>
2026-04-04 20:40:23 +0300

    restored audio source references in scene

--- Assets/Scripts/LandingAreaTrigger.cs
+++ Assets/Scripts/LandingAreaTrigger.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 
 public class LandingAreaTrigger : MonoBehaviour
 {
+    public GameObject missionText;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            Debug.Log("landed successfully");
+            GameObject missile = GameObject.FindWithTag("Missile");
+
+            if (missile == null)
+            {
+                Debug.Log("landed successfully, mission complete congratulations");
+
+                if (missionText != null)
+                {
+                    missionText.SetActive(true);
+                }
+            }
+            else
+            {
+                Debug.Log("Cannot land, threat still active!");
+            }
         }
     }
 }
